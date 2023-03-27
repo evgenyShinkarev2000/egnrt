fa1ba27f4db9781f52b49815a59f8fffea678f74
evgenyShinkarev2000 <evgeny.shinkarev@urfu.me>
2023-03-27 12:23:48 +0500

    Пример в devTools

--- packages/doc-root/src/App.tsx
+++ packages/doc-root/src/App.tsx
@@ -1,6 +1,7 @@
 import { NavLink, Route, Routes } from 'react-router-dom';
 import styles from "./App.module.scss";
 import { ContextExample } from './components/ContextExample';
+import { DevToolsWrongStateExample } from './components/DevToolsWrongStateExample';
 import { FormModelExample } from './components/FormModelExample';
 import { UseEffectStrangeExample } from './components/UseEffectStangeExample';
 
@@ -14,11 +15,13 @@ function App()
       <NavLink to="/form">form exmaple</NavLink>
       <NavLink to="/context">context rerender example</NavLink>
       <NavLink to="/useEffectStrange">useEffect strange example</NavLink>
+      <NavLink to="/devToolsWrongState">dev tools wrong state exmple</NavLink>
       </div>
       <Routes>
         <Route path="/form" Component={FormModelExample} />
         <Route path="/context" Component={ContextExample}/>
         <Route path="/useEffectStrange" Component={UseEffectStrangeExample}/>
+        <Route path="devToolsWrongState" Component={DevToolsWrongStateExample}/>
         <Route path="/" Component={null}></Route>
       </Routes>
     </div>
---
+++ packages/doc-root/src/components/DevToolsWrongStateExample.tsx
@@ -0,0 +1,20 @@
+import React, { useCallback, useState } from "react";
+
+let index = 0;
+// react dev tools видят только один ключ типа symbol, остальные теряются. Если добавить к ним различное описанние, то работант нормально.
+
+export const DevToolsWrongStateExample: React.FC = () =>
+{
+  const [, rerender] = useState(false);
+  const [state] = useState({} as { [key: string | symbol | number]: any });
+  const changeStateField = () => state[Symbol()] = index++;
+  const forceUpdate = () => rerender(prev => !prev);
+  console.log(state);
+
+  return (
+    <div>
+      <button onClick={forceUpdate}>rerender</button>
+      <button onClick={changeStateField}>change state field</button>
+    </div>
+  )
+}
